f86b202c57f306322417b9eac132058a7e05d4bc
Mehdi <moujahedmehdi@gmail.com>
2020-04-11 23:39:33 +0100

    IP addres changed back to 1.4

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -7,7 +7,7 @@ import { from } from "rxjs";
   providedIn: "root",
 })
 export class ProductsService {
-  baseUrl = "http://192.168.1.3:51044/delivery-app/";
+  baseUrl = "http://192.168.1.4:51044/delivery-app/";
 
   constructor(private http: HttpClient) {}
 
