72f01d4b2b16b7473db52ab9037ec2bdff8fd168
Mehdi <moujahedmehdi@gmail.com>
2020-04-17 20:45:47 +0100

    Adding action sheet on profile page

--- src/app/tab-profile/tab-profile.page.ts
+++ src/app/tab-profile/tab-profile.page.ts
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import {
   ModalController,
@@ -10,6 +11,12 @@ import { Subscription } from "rxjs";
 import { ClientsService } from "../services/clients.service";
 import { Client } from "../models/client.model";
 import { DomSanitizer } from "@angular/platform-browser";
+=======
+import { Component, OnInit } from '@angular/core';
+import { ModalController, PopoverController, ActionSheetController } from '@ionic/angular';
+import { ProfileImageModalPage } from '../profile-image-modal/profile-image-modal.page';
+import { PopoverComponentPage } from '../popover-component/popover-component.page';
+>>>>>>> Stashed changes
 
 @Component({
   selector: "app-tab-profile",
@@ -42,6 +49,14 @@ export class TabProfilePage implements OnInit, OnDestroy {
     this.readOnly = true;
   }
 
+<<<<<<< Updated upstream
+=======
+  constructor(private modalController: ModalController, private popoverController: PopoverController, private actionSheetController: ActionSheetController) {
+    this.ButtonDisabled=true;
+    this.readOnly=true;
+   }
+  
+>>>>>>> Stashed changes
   ngOnInit() {
     this.getClient();
     this.clientSubscription = this.clientsService.clientSubject.subscribe(
@@ -68,6 +83,36 @@ export class TabProfilePage implements OnInit, OnDestroy {
     this.readOnly = true;
   }
 
+<<<<<<< Updated upstream
+=======
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photo de profil',
+      buttons: [{
+        text: 'Prendre une photo ',
+        icon: 'camera-outline',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Sélectionner une photo',
+        icon: 'images-outline',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+  
+>>>>>>> Stashed changes
   async presentPopover(ev: any) {
     const popover = await this.popoverController.create({
       component: PopoverComponentPage,
@@ -75,11 +120,19 @@ export class TabProfilePage implements OnInit, OnDestroy {
       translucent: true,
       componentProps: {
         onclick: (test) => {
+<<<<<<< Updated upstream
           if (test == 1) {
             this.readOnly = false;
             this.ButtonDisabled = false;
           } else if (test == 2) {
             console.log("changer image");
+=======
+          if (test==1) {
+            this.readOnly=false;
+            this.ButtonDisabled=false;
+          } else if (test==2){
+            this.presentActionSheet(); 
+>>>>>>> Stashed changes
           } else {
             console.log("déconnexion");
           }
